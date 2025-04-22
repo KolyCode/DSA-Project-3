0f3e6cb4ad8d3151c290294bd25bc9ce02ffb2bd
jd0618 <128635206+jd0618@users.noreply.github.com>
2025-04-22 09:05:26 -0400

    Update plot_tester.py

--- plot_tester.py
+++ plot_tester.py
@@ -1,8 +1,7 @@
-# a non-GUI implementation of pathfinding plots
-# generates a 3d plotly plot in the user's browser
+# a console-input implementation of pathfinding plots
+# displays a 3d plotly plot in the user's browser
 
 from plotter import *
-#import plotly.graph_objects as go
 
 print("Plotter for Path-Finding Algorithms (Dijkstra & A*)")
 dim_in = input("Enter the x and y dimensions of the terrain to be generated as integers separated by a space: ").split()
@@ -14,24 +13,26 @@ nodemap = create_node_map_from_heightmap(heightmap)
 terrain_plot = plotMap3d(heightmap)
 #terrain_plot.show()  # display just terrain
 
+# input start / end
 start_in = input("Enter the x and y dimensions of the starting point, separated by a space: ").split()
 start_in = (int(start_in[0]), int(start_in[1]))
 end_in = input("Enter the x and y dimensions of the destination point, separated by a space: ").split()
 end_in = (int(end_in[0]), int(end_in[1]))
 
-
+start_node = nodemap[int(start_in[0])][int(start_in[1])]
+end_node = nodemap[int(end_in[0])][int(end_in[1])]
 
 # generate paths
 print("""\nDisplaying Terrain and Paths in Plotly... 
       \n\tHold `Left-Click` to rotate
       \n\tUse `Scroll Wheel` to zoom
       \n\tHold `Right-Click` to pan""")
-dists, prev = dijkstra(nodemap, start_in)
-dijk_path = get_path(prev, end_in)
+dists, prev = dijkstra(nodemap, start_node)
+dijk_path = get_path(prev, end_node)
 #dijk_path = [nodemap[0][0], nodemap[1][1], nodemap[2][1], nodemap[2][2]]  # temp : test case
-a_star_path = a_star(start_in, end_in, nodemap)
+a_star_path = a_star(start_node, end_node, nodemap)
 #a_star_path = [nodemap[0][0], nodemap[1][1], nodemap[2][2], nodemap[2][3], nodemap[3][3], nodemap[4][4]]  # temp : test case
 
-
+# add paths to surface
 full_plot = addPathsToMap3d(terrain_plot, dijk_path, a_star_path)
 full_plot.show()
