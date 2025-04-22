9326da0e773fb8436bc77cab8f2cc9da1334e972
jd0618 <128635206+jd0618@users.noreply.github.com>
2025-04-22 11:56:20 +0000

    non-gui app needs path-gen, implemented otherwise

--- plot_tester.py
+++ plot_tester.py
@@ -1,5 +1,5 @@
 # a non-GUI implementation of pathfinding plots
-# generates a plotly plot in the user's browser
+# generates a 3d plotly plot in the user's browser
 
 from plotter import *
 #import plotly.graph_objects as go
@@ -8,6 +8,7 @@ print("Plotter for Path-Finding Algorithms (Dijkstra & A*)")
 dim_in = input("Enter the x and y dimensions of the terrain to be generated as integers separated by a space: ").split()
 
 # generate initial 3D heightmap
+print("\nGenerating terrain with Perlin Noise...")
 heightmap = create_height_map(int(dim_in[0]), int(dim_in[1]))
 nodemap = create_node_map_from_heightmap(heightmap)
 terrain_plot = plotMap3d(heightmap)
@@ -21,13 +22,16 @@ end_in = (int(end_in[0]), int(end_in[1]))
 
 
 # generate paths
+print("""\nDisplaying Terrain and Paths in Plotly... 
+      \n\tHold `Left-Click` to rotate
+      \n\tUse `Scroll Wheel` to zoom
+      \n\tHold `Right-Click` to pan""")
 #dists, prev = dijkstra(nodemap, start_in)
 #dijk_path = get_path(prev, end_in)
-dijk_path = [nodemap[0][0], nodemap[1][1], nodemap[2][1], nodemap[2][2]] # temp : test case
-print(dijk_path)
+dijk_path = [nodemap[0][0], nodemap[1][1], nodemap[2][1], nodemap[2][2]]  # temp : test case
 #a_star_path = a_star(start_in, end_in, nodemap)
-a_star_path = [nodemap[0][0], nodemap[1][1], nodemap[2][2], nodemap[2][3], nodemap[3][3], nodemap[4][4]] # temp : test case
-print(a_star_path)
+a_star_path = [nodemap[0][0], nodemap[1][1], nodemap[2][2], nodemap[2][3], nodemap[3][3], nodemap[4][4]]  # temp : test case
+
 
 full_plot = addPathsToMap3d(terrain_plot, dijk_path, a_star_path)
 full_plot.show()
--- plotter.py
+++ plotter.py
@@ -3,163 +3,136 @@ from map_creation import *  # provides heightmap
 from path_finding import *
 import plotly.graph_objects as go
 
+# color maps for heightmap
+# alternatively, colorscale = "Blackbody", colorscale = "Rainbow"
+elevation_colorscale = [
+    [0.0, "darkblue"],
+    [0.2, "darkgreen"],
+    [0.3, "green"],
+    [0.4, "lightgreen"],
+    [0.5, "yellow"],
+    [0.5, "gray"],
+    [1.0, "white"]
+]
+# resembles Apple Maps Satellite View
+elevation_colorscale_2 = [
+    [0.0, "darkgreen"],
+    [0.2, "green"],
+    [0.4, "tan"],
+    [0.6, "brown"],
+    [0.8, "gray"],
+    [1.0, "white"]
+]
+# resembles Google Satellite Maps
+elevation_colorscale_3 = [
+    [0.0, "green"],
+    [0.2, "lightgreen"],
+    [0.4, "yellow"],
+    [0.6, "saddlebrown"],
+    [0.8, "darkbrown"],
+    [1.0, "white"]
+]
 
 def plotMap3d(heightmap):
     # 3d surface
-    # requires path x, path y, path z (having values of height from heightmap)
     fig = go.Figure()
     fig.add_trace(
         go.Surface(
             z = heightmap,
-            colorscale = "Blackbody"
+            #colorscale = "Blackbody"
+            colorscale = elevation_colorscale_2
         )
     )
     fig.update_layout(
         title = "Surface Plot of Generated Terrain and Paths",
-        scene = dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Height", zaxis=dict(range=[0,255]))
+        scene = dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Elevation", zaxis=dict(range=[0,255]))
     )
     return fig
 
 def addPathsToMap3d(fig, dijk_path, a_star_path):
+    # generate lists of values x,y,z in paths
     path1_x, path1_y, path1_z = [], [], []
     if dijk_path != None:
         for n in range(len(dijk_path)): 
             path1_x.append(dijk_path[n].posx)
             path1_y.append(dijk_path[n].posy)
-            path1_z.append(dijk_path[n].height)
-            #path1_x[n], path1_y[n], path1_z[n] = dijk_path[n].posx, dijk_path[n].posy, dijk_path[n].height
+            path1_z.append(dijk_path[n].height+5)
     path2_x, path2_y, path2_z = [], [], []
     if a_star_path != None:
         for n in range(len(a_star_path)):
             path2_x.append(a_star_path[n].posx)
             path2_y.append(a_star_path[n].posy)
-            path2_z.append(a_star_path[n].height)
-            #path2_x[n], path2_y[n], path2_z[n] = a_star_path[n].posx, a_star_path[n].posy, a_star_path[n].height
+            path2_z.append(a_star_path[n].height+5)
+
+    # plot Dijkstra path
     fig.add_trace(
         go.Scatter3d(
             x = path1_x,
             y = path1_y,
             z = path1_z,
             mode = "lines+markers",
-            line = dict(color="rgba(143, 0, 255, 0.6)", width=4),
-            marker = dict(color="rgba(143, 0, 255, 0.6)", size=8),
+            line = dict(color="rgba(143, 0, 255, 0.6)", width=8),
+            marker = dict(color="rgba(143, 0, 255, 0.6)", size=12),
             name = "Dijkstra"
         )
     )
+    # plot A* path
     fig.add_trace(
         go.Scatter3d(
             x = path2_x,
             y = path2_y,
             z = path2_z,
             mode = "lines+markers",
-            line = dict(color="rgba(153, 187, 38, 0.6)", width=4),
-            marker = dict(color="rgba(153, 187, 38, 0.6)", size=8),
+            line = dict(color="rgba(153, 187, 38, 0.6)", width=8),
+            marker = dict(color="rgba(153, 187, 38, 0.6)", size=12),
             name = "A Star"
         )
     )
+
+    # mark start and end points
+    fig.add_trace(
+        go.Scatter3d(
+            x=[path1_x[0]], y=[path1_y[0]], z=[path1_z[0]],
+            mode="markers+text",
+            marker={"size":12,"color":"green"},
+            text=["Start"],
+            name="Start Point",
+            textposition="top center"
+        )
+    )
+    fig.add_trace(
+        go.Scatter3d(
+            x=[path1_x[-1]], y=[path1_y[-1]], z=[path1_z[-1]],
+            mode="markers+text",
+            marker={"size":12,"color":"red"},
+            text=["End"],
+            name="End Point",
+            textposition="top center"
+        )
+    )
+
+    path_annotation = f"""
+Dijkstra Path Length :  {len(dijk_path)}<br>
+A* Path Length :  {len(a_star_path)}
+"""
+    fig.update_layout(
+        # position legend in bottom left
+        legend=dict(
+            x=0.05,
+            y=0.1
+        ),
+        # print path lengths over plot
+        annotations=[
+            dict(
+                x=0.05, y=0.9,
+                xref="paper", yref="paper",
+                text=path_annotation,
+                font={"size":14,"color":"black"},
+                xanchor="left",
+                showarrow=False,
+                bgcolor="white",
+                bordercolor="white"
+            )
+        ]
+    )
     return fig
-
-
-def plotTerrainAndPaths(as_3d=False):
-    # on push of Plot Paths button
-
-    # this function follows a call to create_maps(x,y)
-    # heightmap: 2D numpy array of weights 0-255
-    # nodemap: 2D array, entries are node objects with position, height, neighbor weight, path presence attributes
-
-    # get x,y,z values from paths
-    dijk_path = None#FIXME
-    a_star_path = None#FIXME
-
-    path1_x, path1_y, path1_z = [], [], []
-    if dijk_path != None:
-        for n in range(len(dijk_path)): 
-            path1_x[n], path1_y[n], path1_z[n] = dijk_path[n].posx, dijk_path[n].posy, dijk_path[n].height
-    path2_x, path2_y, path2_z = [], [], []
-    if a_star_path != None:
-        for n in range(len(a_star_path)):
-            path2_x[n], path2_y[n], path2_z[n] = a_star_path[n].posx, a_star_path[n].posy, a_star_path[n].height
-
-    #print(f"heightmap size : {heightmap.shape}")
-    print(f"dijkstra path length : {len(path1_z)}\t a_star path length : {len(path2_z)}\t as_3d : {as_3d}")
-    if as_3d == False:
-        # heatmap plot
-        # requires x, y arrays of paths
-        fig = go.Figure()
-        fig.add_trace(
-            go.Heatmap(
-                z = heightmap,
-                colorscale = "Blackbody",
-                text = [[str(height) for height in r] for r in heightmap],
-                texttemplate="%{text}",
-                textfont={"size":15}
-            )
-        )
-        fig.add_trace( # path 1
-            go.Scatter(
-                x = path1_x,
-                y = path1_y,
-                mode = "lines+markers",
-                line = dict(color="rgba(143, 0, 255, 0.6)", width=4),
-                marker = dict(color="rgba(143, 0, 255, 0.6)", size=8),
-                name = "Dijkstra"
-            )
-        )
-        fig.add_trace( # path 2
-            go.Scatter(
-                x = path2_x,
-                y = path2_y,
-                mode = "lines+markers",
-                line = dict(color="rgba(153, 187, 38, 0.6)", width=4),
-                marker = dict(color="rgba(153, 187, 38, 0.6)", size=8),
-                name = "A Star"
-            )
-        )
-        fig.update_layout(
-            title = "Heatmap of Generated Terrain and Paths",
-            xaxis = dict(title="X"),
-            yaxis = dict(title="Y")
-        )
-    else: 
-        # 3d surface
-        # requires path x, path y, path z (having values of height from heightmap)
-        fig = go.Figure()
-        fig.add_trace(
-            go.Surface(
-                z = heightmap,
-                colorscale = "Blackbody"
-            )
-        )
-        fig.add_trace(
-            go.Scatter3d(
-                x = path1_x,
-                y = path1_y,
-                z = path1_z,
-                mode = "lines+markers",
-                line = dict(color="rgba(143, 0, 255, 0.6)", width=4),
-                marker = dict(color="rgba(143, 0, 255, 0.6)", size=8),
-                name = "Dijkstra"
-            )
-        )
-        fig.add_trace(
-            go.Scatter3d(
-                x = path2_x,
-                y = path2_y,
-                z = path2_z,
-                mode = "lines+markers",
-                line = dict(color="rgba(153, 187, 38, 0.6)", width=4),
-                marker = dict(color="rgba(153, 187, 38, 0.6)", size=8),
-                name = "A Star"
-            )
-        )
-        fig.update_layout(
-            title = "Surface Plot of Generated Terrain and Paths",
-            scene = dict(xaxis_title="X", yaxis_title="Y", zaxis_title="Height")
-        )
-        
-    return fig
-
-def clearPaths(): 
-    # on push of Reset button
-    pass
-    
